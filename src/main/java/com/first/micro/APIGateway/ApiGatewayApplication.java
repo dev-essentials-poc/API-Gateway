6e4b20951ae73ad404cb5a04d3518eba058059d3
Essentials Git Repo <58247890+dev-essentials-poc@users.noreply.github.com>
2019-12-13 14:31:51 +0530

    Added debug statement to api-gateway

--- src/main/java/com/first/micro/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/first/micro/APIGateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("Welcome to api-gateway micro-service ...!");
 	}
 
 }
